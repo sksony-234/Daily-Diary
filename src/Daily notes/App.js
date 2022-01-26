a4af6b71e0c3770b0afb4ac62cf8212745075c6e
sksony <sksony2698@gmail.com>
2022-01-26 08:35:13 +0530

    first commit

---
+++ src/Daily notes/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>
+            <h1>Siddharth Sony.</h1> 
+            <pre>Sunday  26/01/2022</pre>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci corporis eos blanditiis, quibusdam natus deleniti quidem qui, facilis voluptatem rem, magni inventore reprehenderit?</p>
+        </>
+    )
+}
+
+export default App
