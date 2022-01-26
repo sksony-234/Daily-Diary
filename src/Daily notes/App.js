ddfd1f0ce2a08aa7c0e0b3ea03821006a14427d0
sksony <sksony2698@gmail.com>
2022-01-26 09:21:09 +0530

    chabge2

--- src/Daily notes/App.js
+++ src/Daily notes/App.js
@@ -6,6 +6,7 @@ const App = () => {
             <h1>Siddharth Sony.</h1> 
             <pre>Sunday  26/01/2022</pre>
             <p>Hii there, Here I used to asign My daily basis task I hope it Help me to improove My productivity and Sensiority.</p>
+            <h3>I done it. I have completed my todays task.</h3>
         </>
     )
 }
